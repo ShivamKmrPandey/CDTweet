42571d6af25391bce9e0c312c5bda6cdb9955e8a
ShivamKmrPandey <104768132+ShivamKmrPandey@users.noreply.github.com>
2022-05-03 03:07:04 +0530

    Delete tweet-app directory

--- tweet-app/src/services/auth.header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-  const user = JSON.parse(localStorage.getItem('user'));
-
-  
-    console.log(user.token);
-    let authtoken = user.token;
-    return authtoken; // for Spring Boot back-end
-  
-}
